1dcebf2463a4669b3213768557b381b0f85eef27
Afsaneh Habibi <a.habibi1394@gmail.com>
2024-03-26 10:33:58 -0400

    Add test for authentication part of module

---
+++ server/internal/module/auth._test.go
@@ -0,0 +1,38 @@
+package module
+
+import (
+	"testing"
+
+	"github.com/AfsanehHabibi/neveshtedan/graph/model"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWhenNewUserCreatesItReceivesToken(t *testing.T) {
+	token, err := module.CreateUser(ctx, model.NewUser{Username: "Ahmad", Password: "03jf9efk"})
+	assert.NoError(t, err)
+	assert.NotEmpty(t, token)
+}
+
+func TestWhenExistentUserLogsInItReceivesToken(t *testing.T) {
+	username := "Ahmad"
+	password := "03jf9efk"
+	token, err := module.CreateUser(ctx, model.NewUser{Username: username, Password: password})
+	assert.NoError(t, err)
+	assert.NotEmpty(t, token)
+
+	tokenL, err := module.Login(ctx, model.Login{Username: username, Password: password})
+	assert.NoError(t, err)
+	assert.NotEmpty(t, tokenL)
+}
+
+func TestWhenWrongPasswordEnteredLoginReturnsError(t *testing.T) {
+	username := "Ahmad"
+	password := "03jf9efk"
+	token, err := module.CreateUser(ctx, model.NewUser{Username: username, Password: password})
+	assert.NoError(t, err)
+	assert.NotEmpty(t, token)
+
+	tokenL, err := module.Login(ctx, model.Login{Username: username, Password: "wrong"})
+	assert.Error(t, err)
+	assert.Empty(t, tokenL)
+}
---
+++ server/internal/module/main_test.go
@@ -0,0 +1,31 @@
+package module
+
+import (
+	"context"
+	"log"
+	"testing"
+
+	"github.com/AfsanehHabibi/neveshtedan/graph/model"
+	"github.com/AfsanehHabibi/neveshtedan/internal/auth"
+	"github.com/AfsanehHabibi/neveshtedan/pkg/logic"
+	"github.com/AfsanehHabibi/neveshtedan/pkg/repository/postgres/util/test"
+)
+
+var (
+	ctx    = context.Background()
+	module logic.Neveshtedan
+)
+
+func TestMain(m *testing.M) {
+	con := test.PreparePostgresForTest()
+	module = NewNeveshtedanModule(con)
+	m.Run()
+}
+
+func logsBasicUserInAndFillContext() context.Context {
+	_, err := module.CreateUser(ctx, model.NewUser{Username: "Ahmad", Password: "03jf9efk"})
+	if err != nil {
+		log.Fatalln("failed to setup test user")
+	}
+	return auth.AddUserToContext(ctx, 0)
+}
